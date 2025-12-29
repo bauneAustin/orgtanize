6d8cd5a8bd352f70f4eee9dad08cd8b0cbebd7cd
bauneAustin <abaune.dev@gmail.com>
2025-12-29 13:08:40 -0700

    quick cleanup

--- src/components/CalendarGrid.tsx
+++ src/components/CalendarGrid.tsx
@@ -12,7 +12,7 @@ const CalendarGrid: React.FC = () => {
     const getDays = (): ReactElement[] => {
         return Array.from({ length: 7 }, (_v, index) => {
             const { dayName, dayNumber, isToday } = currentWeekDetails[index];
-            return <span className={`flex flex-col items-center justify-center border-r p-2 ${isToday ? 'bg-blue-50 font-semibold' : ''}`} key={dayName}>
+            return <span className={`flex flex-col items-center justify-center p-2 rounded-md ${isToday ? 'bg-blue-100 font-semibold' : ''}`} key={dayName}>
                 <span>{dayName}</span>
                 <span className='block'>{dayNumber}</span>
             </span>;
@@ -20,7 +20,7 @@ const CalendarGrid: React.FC = () => {
     };
 
     return (
-        <div className='flex-1 overflow-hidden bg-accent-light p-6 rounded-md min-w-0'>
+        <div className='flex-1 overflow-hidden p-6 rounded-md min-w-0'>
             <div className='font-bold text-3xl flex mb-5'>
                 {monthName} {year} <span className='flex relative top-1'><ChevronLeft className='cursor-pointer w-8 h-8' /> <ChevronRight className='cursor-pointer w-8 h-8' /></span>
             </div>
